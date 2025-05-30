f62ebc37454f191f537a31c0c6cdb1cf30ba27ac
Carlos A. <carlosvsk@gmail.com>
2025-05-30 14:52:53 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -256,7 +256,6 @@ xmlns="http://www.w3.org/2000/svg"
 
           args,
         }}/>, 
-        
 
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
@@ -312,21 +311,7 @@ if (resultado >= 0) {
 
             args,
           }}/>
-        , (...args:any) => <Elements.IptTxtEdit pass={{
-          propsArray: [{}],
-
-          stylesArray: [`{ marginBottom: 20 }`],
-
-          path: [`all.forms.form1.edu`],
-
-          funcsArray: [async (...args) =>
-        functions.setVar({ args, pass:{
-          keyPath: [`all.forms.form1.edu`],
-          value: ['no value']
-        }})],
-
-          args,
-        }}/>],
+        ],
 
           functions:[()=>{}],
 
