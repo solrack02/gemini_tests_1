6a933bb8100e20656c3ab36efc02286f9d006db8
Carlos A. <carlosvsk@gmail.com>
2025-05-30 13:54:53 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -287,7 +287,7 @@ const resultado = toNum(irRet) - irDevido;
 
 
 if (resultado >= 0) {
-  console.log('Restituição: R + resultado.toFixed(2) );
+  console.log("Restituição: R$" + resultado.toFixed(2) );
 } else {
   console.log('Imposto a pagar: R +  Math.abs(resultado).toFixed(2) );
 }
